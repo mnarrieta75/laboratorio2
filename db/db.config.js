0add79f9a35e0384f612bf48a67344351ecb9f62
mnarrieta75 <mnarrieta@gmail.com>
2023-07-04 16:45:50 +0200

    Commit acceso a DDBB MongoDB produccion

--- db/db.config.js
+++ db/db.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    url: "mongodb://localhost:27017/laboratorio"
+    //url: "mongodb://localhost:27017/laboratorio"
+    url: "mongodb+srv://mnarrieta75:<password>@mongodb.r1umurl.mongodb.net/"
   };
