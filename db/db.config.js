8fc51711bbf13348e95f5a8fb94846e0af8d546a
mnarrieta75 <mnarrieta@gmail.com>
2023-07-04 16:53:05 +0200

    Commit acceso a DDBB MongoDB produccion intento23

--- db/db.config.js
+++ db/db.config.js
@@ -1,4 +1,5 @@
 module.exports = {
     //url: "mongodb://localhost:27017/laboratorio"
-    url: "mongodb+srv://mnarrieta75:fF0CcggGY43MfS2P@mongodb.r1umurl.mongodb.net/"
+    //url: "mongodb+srv://mnarrieta75:fF0CcggGY43MfS2P@mongodb.r1umurl.mongodb.net/"
+    url: "mongodb://mnarrieta75:fF0CcggGY43MfS2P@mongodb.r1umurl.mongodb.net/"
   };
